d9c9b083721d37750464aa71d3161a118251273c
SL-Gundam <slgundam@gmail.com>
2011-08-05 17:56:51 +0200

    USERLEN and REALLEN renamed to DB_FIELD_SIZE_USERNAME and DB_FIELD_SIZE_REALNAME
    Minimum version is now 1.2.6 because of the above change

--- core/mail_api.php
+++ core/mail_api.php
@@ -1009,9 +1009,9 @@ class ERP_mailbox_api
 				$t_username = $p_user_info[ 'name' ];
 		}
 
-		if ( utf8_strlen( $t_username ) > USERLEN )
+		if ( utf8_strlen( $t_username ) > DB_FIELD_SIZE_USERNAME )
 		{
-			$t_username = utf8_substr( $t_username, 0, USERLEN );
+			$t_username = utf8_substr( $t_username, 0, DB_FIELD_SIZE_USERNAME );
 		}
 
 		if ( user_is_name_valid( $t_username ) && user_is_name_unique( $t_username ) )
@@ -1023,9 +1023,9 @@ class ERP_mailbox_api
 		$t_username = strtolower( str_replace( array( '@', '.', '-' ), '_', $p_user_info[ 'email' ] ) );
 		$t_rand = '_' . mt_rand( 1000, 99999 );
 
-		if ( utf8_strlen( $t_username . $t_rand ) > USERLEN )
+		if ( utf8_strlen( $t_username . $t_rand ) > DB_FIELD_SIZE_USERNAME )
 		{
-			$t_username = utf8_substr( $t_username, 0, ( USERLEN - strlen( $t_rand ) ) );
+			$t_username = utf8_substr( $t_username, 0, ( DB_FIELD_SIZE_USERNAME - strlen( $t_rand ) ) );
 		}
 
 		$t_username = $t_username . $t_rand;
@@ -1068,9 +1068,9 @@ class ERP_mailbox_api
 				$t_realname = $p_user_info[ 'name' ];
 		}
 
-		if ( utf8_strlen( $t_realname ) > REALLEN )
+		if ( utf8_strlen( $t_realname ) > DB_FIELD_SIZE_REALNAME )
 		{
-			$t_realname = utf8_substr( $t_realname, 0, REALLEN );
+			$t_realname = utf8_substr( $t_realname, 0, DB_FIELD_SIZE_REALNAME );
 		}
 
 		if ( user_is_realname_valid( $t_realname ) && user_is_realname_unique( $p_username, $t_realname ) )
