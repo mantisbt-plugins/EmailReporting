8923d7efc88ba4dd005246c2f36dae8a0abe91fb
SL-Gundam <slgundam@gmail.com>
2011-10-06 21:57:23 +0200

    Small adjustment for variable scope

--- scripts/bug_report_mail.php
+++ scripts/bug_report_mail.php
@@ -1,11 +1,11 @@
 <?php
-$t_dir_emailreporting_adjust = dirname( __FILE__ ) . '/../../../';
-require_once( $t_dir_emailreporting_adjust . 'core.php' );
+$GLOBALS[ 't_dir_emailreporting_adjust' ] = dirname( __FILE__ ) . '/../../../';
+require_once( $GLOBALS[ 't_dir_emailreporting_adjust' ] . 'core.php' );
 
 $t_tmp_plugin_page = plugin_page( 'bug_report_mail', TRUE, 'EmailReporting' );
 $t_tmp_plugin_page = explode( '?', $t_tmp_plugin_page, 2 );
 $t_tmp_plugin_page[ 1 ] = explode( '=', $t_tmp_plugin_page[ 1 ], 2 );
 
 $_GET[ $t_tmp_plugin_page[ 1 ][ 0 ] ] = $t_tmp_plugin_page[ 1 ][ 1 ];
-require_once( $t_dir_emailreporting_adjust . $t_tmp_plugin_page[ 0 ] );
+require_once( $GLOBALS[ 't_dir_emailreporting_adjust' ] . $t_tmp_plugin_page[ 0 ] );
 ?>
