4ed18f5da02e426d044d64d00e6167de1cc80349
SL-Gundam <slgundam@gmail.com>
2013-03-02 15:27:12 +0100

    Small adjustments to manage rule page

--- pages/manage_rule.php
+++ pages/manage_rule.php
@@ -74,15 +74,16 @@ ERP_output_config_option( 'description', 'string', $t_rule );
 
 ERP_output_config_option( NULL, 'empty' );
 ERP_output_config_option( 'rule_conditions', 'header' );
-ERP_output_config_option( 'cond_issue_creation_method', 'dropdown_any', $t_rule, 'print_descriptions_option_list', array( 'emailreporting', 'mantisbt' ) );// TODO think about name and think this over whether selected mailboxes should be here or on the manage mailboxes page
+ERP_output_config_option( 'cond_issue_creation_method', 'dropdown_multiselect_any', $t_rule, 'print_descriptions_option_list', array_keys( $g_plugin_cache ) );
 ERP_output_config_option( 'cond_mailbox', 'dropdown_multiselect_any', $t_rule, 'print_descriptions_option_list', plugin_config_get( 'mailboxes' ) );
-ERP_output_config_option( 'cond_issue_issue_issuenote', 'dropdown_any', $t_rule, 'print_descriptions_option_list', array( 'newnote', 'newissue' ) ); //TODO note or new issue
+ERP_output_config_option( 'cond_issue_issue_issuenote', 'dropdown_multiselect_any', $t_rule, 'print_descriptions_option_list', array( 'newnote', 'newissue', 'newattachment' ) );
 ERP_output_config_option( 'cond_issue_reporter', 'dropdown_multiselect_any', $t_rule, 'print_reporter_option_list' );
 ERP_output_config_option( 'cond_issue_project', 'dropdown_multiselect_any', $t_rule, 'print_projects_option_list' );
 ERP_output_config_option( 'cond_issue_category', 'dropdown_multiselect_any', $t_rule, 'print_global_category_option_list' );
 ERP_output_config_option( 'cond_issue_priority', 'dropdown_multiselect_any', $t_rule, 'print_priority_option_list' );
 ERP_output_config_option( 'cond_issue_summary', 'string', $t_rule );
 ERP_output_config_option( 'cond_issue_description', 'string', $t_rule );
+ERP_output_config_option( 'recorddisabled', 'empty' );
 
 ERP_output_config_option( NULL, 'empty' );
 ERP_output_config_option( 'rule_actions', 'header' );
@@ -111,7 +112,7 @@ ERP_output_config_option( 'rules', 'header', 'manage_mailbox' );
 
 ERP_output_config_option( 'rule_action', 'radio_buttons', $f_rule_action, 'print_rule_action_radio_buttons', $t_rules );
 ERP_output_config_option( 'select_rule', 'dropdown', $f_select_rule, 'print_descriptions_option_list', $t_rules );
-ERP_output_config_option( 'disabled', 'empty' );
+ERP_output_config_option( 'recorddisabled', 'empty' );
 
 ERP_output_config_option( 'select_rule', 'submit' );
 
