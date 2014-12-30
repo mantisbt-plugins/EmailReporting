72e7b04750467a42e8e2c83c2c38f3722e2ead09
SL-Gundam <slgundam@gmail.com>
2014-12-30 21:04:29 +0100

    Added error handling for plugin schema updates

--- scripts/bug_report_mail.php
+++ scripts/bug_report_mail.php
@@ -2,7 +2,17 @@
 $GLOBALS[ 't_dir_emailreporting_adjust' ] = dirname( __FILE__ ) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR;
 require_once( $GLOBALS[ 't_dir_emailreporting_adjust' ] . 'core.php' );
 
-$t_tmp_plugin_page = plugin_page( 'bug_report_mail', TRUE, 'EmailReporting' );
+$t_basename = 'EmailReporting';
+$t_pagename = 'bug_report_mail';
+
+if( plugin_needs_upgrade( $g_plugin_cache[ $t_basename ] ) )
+{
+	error_parameters( $t_basename );
+	echo error_string( ERROR_PLUGIN_UPGRADE_NEEDED );
+	exit;
+}
+
+$t_tmp_plugin_page = plugin_page( $t_pagename, TRUE, $t_basename );
 $t_tmp_plugin_page = explode( '?', $t_tmp_plugin_page, 2 );
 $t_tmp_plugin_page[ 1 ] = explode( '=', $t_tmp_plugin_page[ 1 ], 2 );
 
