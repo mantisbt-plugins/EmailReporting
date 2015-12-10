1a5704b9e93d66faa143512f9f4efab4063bf958
SL-Gundam <slgundam@gmail.com>
2015-12-11 00:39:03 +0100

    Trivial change - Reordered the subject line regexes

--- core/mail_api.php
+++ core/mail_api.php
@@ -1300,12 +1300,12 @@ class ERP_mailbox_api
 		// strict is default incase the setting contains an invalid value
 		switch ( $this->_mail_subject_id_regex )
 		{
-			case 'balanced':
-				$t_subject_id_regex = "/\[(?P<project>[^\]]+\s|)0*(?P<id>[0-9]+)\]/u";
-				break;
-
 			case 'relaxed':
 				$t_subject_id_regex = "/\[(?P<project>[^\]]*\s|)0*(?P<id>[0-9]+)\s*\]/u";
+				break;
+
+			case 'balanced':
+				$t_subject_id_regex = "/\[(?P<project>[^\]]+\s|)0*(?P<id>[0-9]+)\]/u";
 				break;
 
 			case 'strict':
