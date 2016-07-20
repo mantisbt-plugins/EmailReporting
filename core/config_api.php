ba449644b90157455e4be708afab8f3642d95191
SL-Gundam <slgundam@gmail.com>
2016-07-20 23:07:13 +0200

    Fix possible fatal error on return value

--- core/config_api.php
+++ core/config_api.php
@@ -909,7 +909,8 @@ if ( !function_exists( 'test_database_utf8' ) )
 		{
 			foreach ( $t_user_ids AS $t_single_user_id )
 			{
-				if ( !user_exists( $t_single_user_id ) || empty( user_get_accessible_projects( $t_single_user_id ) ) )
+				$t_user_get_accessible_projects = user_get_accessible_projects( $t_single_user_id );
+				if ( !user_exists( $t_single_user_id ) || empty( $t_user_get_accessible_projects ) )
 				{
 					echo '<option value="' . $t_single_user_id . '" selected class="negative">' . plugin_lang_get( 'missing_user', 'EmailReporting' ) . ': ' . $t_single_user_id . '</option>';
 				}
