419f4c72c96a3b343460776fefe4d1376a1585ef
Daniele Verducci <daniele.verducci@ag2.it>
2022-02-08 17:49:32 +0100

    Revert "EmailReporting answers with a subject compatible with the one used by Mantis" (asked by gtozzi)
    This reverts commit 8ee5c2b8cf300b7970bc9f521c9186de0f1e3175.

--- core/mail_api.php
+++ core/mail_api.php
@@ -2045,17 +2045,13 @@ class ERP_mailbox_api
 	# Fills the placeholders with data from the issue
 	# Supported placeholders:
 	#   %bugid% Mantis numeric bug identifier
-	#   %leadingzerosbugid% Mantis numeric bug identifier, with leading zeros
-	#     (as presented in the issue's ID field)
 	#   %bugtitle% Bug title
 	#   %reporteremail% Reporter email address
 	#   %reportername% Reporter name
 	#   %emailaddr% Plugin's monitored email address that received the report
-	#   %projectname% Name of the project the bug is assigned to
 	private function fillPlaceholders($template, $email, $bugData) {
 		return str_replace([
 			'%bugid%',
-			'%leadingzerosbugid%',
 			'%bugtitle%',
 			'%reporteremail%',
 			'%reportername%',
@@ -2063,19 +2059,16 @@ class ERP_mailbox_api
 			'%emailsubject%',
 			'%emailbody%',
 			'%emaildate%',
-			'%projectname%',
 		],
 		[
 			$bugData->id,
-			sprintf('%07d', $bugData->id),
 			$bugData->summary,
 			$email['From_parsed']['email'],
 			$email['From_parsed']['name'],
 			$this->getPluginEmailAddr(),
 			$email['Subject'],
 			$this->quoteMailText($email['X-Mantis-Body']),
-			$email['Date_parsed'] ? strftime("%d/%m/%y %R", $email['Date_parsed']) : $email['Date'],
-			project_get_name($bugData->project_id),
+			$email['Date_parsed'] ? strftime("%d/%m/%y %R", $email['Date_parsed']) : $email['Date']
 		],
 		$template);
 	}
