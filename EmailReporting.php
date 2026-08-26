2ef13ec9432eaa7a95457dab13ef3a3ecf8514bb
SL-Gundam <SL-Gundam@users.noreply.github.com>
2026-08-26 23:00:35 +0200

    Update version number

--- EmailReporting.php
+++ EmailReporting.php
@@ -11,7 +11,7 @@ class EmailReportingPlugin extends MantisPlugin
 		$this->description = plugin_lang_get( 'plugin_description' );
 		$this->page = 'manage_config';
 
-		$this->version = '0.12.0-DEV';
+		$this->version = '0.12.0';
 		$this->requires = array(
 			'MantisCore' => '2.0.0',
 		);
